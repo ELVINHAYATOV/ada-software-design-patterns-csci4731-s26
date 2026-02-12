53640124f105206ea893b8b0f586f200bd754ff4
ELVINHAYATOV <ehayatov17606@ada.edu.az>
2026-02-12 09:18:06 +0400

    Update App.java

--- src/main/java/az/edu/ada/modules/module04/lecture06/App.java
+++ src/main/java/az/edu/ada/modules/module04/lecture06/App.java
@@ -2,5 +2,10 @@ package az.edu.ada.modules.module04.lecutre06;
 
 public class App {
 
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		Button button = Button();
+		Checkbox checkbox = ChechBox();
+		// Assume WindowManager is external library
+		WindowManager.shared.addViews([checkbox, button]);
+	}
 }
